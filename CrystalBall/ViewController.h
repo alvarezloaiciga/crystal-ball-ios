054b23160fa42a108fa21a3929ffeaebff1830a3
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-03-17 13:37:52 -0600

    Initial Commit

---
+++ CrystalBall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CrystalBall
+//
+//  Created by José Andrés Alvarez Loáiciga on 3/17/13.
+//  Copyright (c) 2013 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CrystalBall/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CrystalBall
+//
+//  Created by José Andrés Alvarez Loáiciga on 3/17/13.
+//  Copyright (c) 2013 com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
